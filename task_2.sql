fd1a48ef937175e3239d3aef4cbe6e035b5322bb
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2025-07-01 12:11:41 +0300

    aa

--- task_2.sql
+++ task_2.sql
@@ -3,7 +3,7 @@ author_id INT AUTO_INCREMENT PRIMARY KEY,
 author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE books (
+CREATE TABLE Books (
 book_id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(130) NOT NULL,
 author_id INT,
@@ -12,21 +12,21 @@ publication_date DATE,
 FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
 customer_id INT AUTO_INCREMENT PRIMARY KEY,
 customer_name VARCHAR(215) NOT NULL,
 email VARCHAR(215),
 address TEXT
 );
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
 order_id INT AUTO_INCREMENT PRIMARY KEY,
 customer_id INT,
 order_date DATE,
 FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
 orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
 order_id INT,
 book_id INT,
