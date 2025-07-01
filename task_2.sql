a3ed9903aadc1aeceb2fccca1035664837f69a75
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2025-07-01 12:12:51 +0300

    aa

--- task_2.sql
+++ task_2.sql
@@ -31,6 +31,6 @@ orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
 order_id INT,
 book_id INT,
 quantity DOUBLE,
-FOREIGN KEY (order_id) REFERENCES orders(order_id),
-FOREIGN KEY (book_id) REFERENCES books(book_id)
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
